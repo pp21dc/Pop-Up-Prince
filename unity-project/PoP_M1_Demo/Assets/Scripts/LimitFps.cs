874595c087ea83f932fa8726426672464421f46e
Brendan Kane <brendankane010@gmail.com>
2023-01-23 18:57:45 -0500

    Upload Unity Project

---
+++ unity-project/PoP_M1_Demo/Assets/Scripts/LimitFps.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitFps : MonoBehaviour
+{
+    //float x = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Debug.Log(x += (1f/Application.targetFrameRate));
+        if (x >= 1f)
+        {
+            Debug.Log("Hit");
+            x = 0f;
+            
+        }
+        */
+    }
+}
