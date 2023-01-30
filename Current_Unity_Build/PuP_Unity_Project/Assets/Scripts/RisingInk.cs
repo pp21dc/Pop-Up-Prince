fc5d07bc253e0189f46703a0f8c9bb6bdf637351
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2023-01-30 13:58:43 -0500

    Created basic Rising Ink
    created script for a prototype rising ink, and made some edits to my scene

---
+++ Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/RisingInk.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingInk : MonoBehaviour
+{
+
+    /**
+     * Script for the rising ink mechanic,
+     * 
+     * put this script on the rising ink that is under the empty game object
+     */
+
+    float startPos;
+    public float moveSpeed = 3f;
+    public static bool canRise = false;
+    //public float waitTime = 5f;
+    public float heightMultiplier = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canRise == true)
+        {
+            //move object up
+            transform.Translate(Vector3.up * (moveSpeed * Time.deltaTime), Space.World);
+
+            if (transform.position.y > (startPos * heightMultiplier))
+            {
+                canRise = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //set player position back to respawn point
+        }
+    }
+
+}
