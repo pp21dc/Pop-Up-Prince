c4efc77fd16b4b3adf22ade524f92e269ea2a7e4
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2023-02-14 20:18:40 -0500

    Edited Rising Ink
    Now rising ink resets to start position and waits for player to rise again

--- Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/RisingInk.cs
+++ Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/RisingInk.cs
@@ -48,6 +48,9 @@ public class RisingInk : MonoBehaviour
         if (collider.gameObject.tag == "Player")
         {
             //PM.Respawn();
+            transform.position.y = startPos;
+            canRise = false;
+
         }
     }
 
