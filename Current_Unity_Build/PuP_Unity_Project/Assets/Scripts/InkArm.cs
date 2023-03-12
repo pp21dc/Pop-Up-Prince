8f622b20b9aee7c4843af01773a349d19c2bb32b
Patrick Perry <patrickperry713@gmail.com>
2023-03-12 16:54:16 -0400

    Revert "Dealt with gate prefab merge conflict"
    This reverts commit 8a00cad2f12c62cc3c257ce2b5a73c6f3c86965f, reversing
    changes made to ef9d977c02e6a5c631955aaeac3b77be91e06dc8.

--- Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/InkArm.cs
+++ Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/InkArm.cs
@@ -15,31 +15,28 @@ public class InkArm : MonoBehaviour
     public float ARM_SPEED = 5f;
     public float ARM_REACH = 2f;
     public bool RETURN = false;
-    public GrabScript GS;
+    GrabScript GS;
 
     // Start is called before the first frame update
     void Start()
     {
-        //GS = gameObject.GetComponentInChildren<GrabScript>();
+        GS = gameObject.GetComponentInChildren<GrabScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null && PFC != null && GS != null)
+        if (player != null && PFC != null)
         {
             player_position = new Vector3(player.transform.position.x, player.transform.position.y + PFC.TR.transform.localPosition.y / 2, player.transform.position.z);
             Vector3 moveTo = Vector3.MoveTowards(transform.position, player_position, ARM_SPEED * Time.deltaTime);
             Vector3 moveToA = Vector3.MoveTowards(transform.position, ANCHOR.position, ARM_SPEED * Time.deltaTime);
             if (move && (!Stretched(moveTo)) && !GS.grabbed)
             {
-                Debug.Log("Move To Player");
                 transform.position = moveTo;
-                transform.LookAt(player);
             }
             else if (!move || GS.grabbed)
             {
-                Debug.Log("Move To Anchor");
                 transform.position = moveToA;
             }
         }
@@ -74,18 +71,13 @@ public class InkArm : MonoBehaviour
             player = other.transform;
             //player = other.transform;
             move = true;
-            Debug.Log("Enter");
+            //Debug.Log("Enter");
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            move = true;
-            Debug.Log("Stay");
-        }
-           
+        move = true;    
     }
 
     private void OnTriggerExit(Collider other)
@@ -95,7 +87,6 @@ public class InkArm : MonoBehaviour
             PFC = other.gameObject.GetComponent<PrinceFootCollider>();
             player = other.transform;
             move = false;
-            Debug.Log("Exit");
         }
     }
 
