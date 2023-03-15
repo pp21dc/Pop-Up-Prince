5495fc651c941cc21e706a7e0411096a33bee987
Brendan Kane <54537957+FlerBaDet@users.noreply.github.com>
2023-03-15 17:16:59 -0400

    ArmTests

--- Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/InkArm.cs
+++ Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/InkArm.cs
@@ -15,8 +15,14 @@ public class InkArm : MonoBehaviour
     //public Collider GRABBER;
     public float ARM_SPEED = 5f;
     public float ARM_REACH = 2f;
+    public float timeToReturn = 10f;
+    public float timerRate = 0.1f;
     public bool RETURN = false;
+    public bool timerOn = false;
     GrabScript GS;
+
+    float timerx = 0;
+    bool reachLock = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,17 +38,33 @@ public class InkArm : MonoBehaviour
             player_position = new Vector3(player.transform.position.x, player.transform.position.y + PFC.TR.transform.localPosition.y / 2, player.transform.position.z);
             Vector3 moveTo = Vector3.MoveTowards(transform.position, player_position, ARM_SPEED * Time.deltaTime);
             Vector3 moveToA = Vector3.MoveTowards(transform.position, ANCHOR.position, ARM_SPEED * Time.deltaTime);
-            if (move && (!Stretched(moveTo)) && !GS.grabbed)
+            if (move && (!Stretched(moveTo)) && !GS.grabbed && !reachLock)
             {
-                //Debug.Log(move);
-                //Debug.Log(GS.grabbed);
+                Debug.Log("TARGET: PLAYER");
                 transform.position = moveTo;
             }
-            else if (!move | GS.grabbed)
+            if ((!move | GS.grabbed) || reachLock)
             {
-                //Debug.Log("ANCHOR");
+                Debug.Log("TARGET: ANCHOR");
                 transform.position = moveToA;
+                if (reachLock && timerx < 10 && timerOn) 
+                {
+                    Debug.Log("COUNTING");
+                    timerx += 0.1f; 
+                }
+                else 
+                {
+                    timerx = 0; 
+                    reachLock = false;  
+                }
+ 
             }
+            if ((Stretched(moveTo)) && !GS.grabbed && timerOn)
+            {
+                Debug.Log("LOCK");
+                reachLock = true;
+            }
+            //Debug.Log("MOVE: " + move);
         }
     }
 
