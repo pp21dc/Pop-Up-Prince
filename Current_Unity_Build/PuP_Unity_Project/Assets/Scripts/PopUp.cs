59079f69ff6abacabc0884b6ee3ed640dcfc8a5b
Patrick Perry <patrickperry713@gmail.com>
2023-03-29 16:49:14 -0400

    finished modifying popups in tutorial

--- Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/PopUp.cs
+++ Current_Unity_Build/PuP_Unity_Project/Assets/Scripts/PopUp.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class PopUp : MonoBehaviour
 {
     bool pop = false;
+    bool finished = false;
+
     float FPS = 60;
     public float POP_SPEED = 64f;
     public float POP_TIME = 1f;
 
+    public GameObject popUp;
+
+    HingeJoint hinge;
+    float timer = 0;
     private void Start()
     {
         //FPS = Application.targetFrameRate;
+         hinge = GetComponent<HingeJoint>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.rotation.x < 0 && pop)
-        {
             //Debug.Log(transform.rotation.x);
-            transform.Rotate((POP_SPEED * Time.deltaTime), 0, 0);
-        }
+            
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,6 +34,7 @@ public class PopUp : MonoBehaviour
         if (other.transform.tag == "Player")
         {
             pop = true;
+            hinge.useSpring = true;
         }
     }
 
