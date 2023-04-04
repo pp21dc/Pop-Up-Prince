ef6ad8fe531d8ed34a59c2112df0c07c837e38e4
Patrick Perry <patrickperry713@gmail.com>
2023-04-04 00:44:06 -0400

    added transparency/updated ink animation

--- Current_Unity_Build/PuP_Unity_Project/Assets/VFX/Shaders/Ink/Ink_Test_3.cs
+++ Current_Unity_Build/PuP_Unity_Project/Assets/VFX/Shaders/Ink/Ink_Test_3.cs
@@ -7,17 +7,22 @@ public class Ink_Test_3 : MonoBehaviour
 
     public Material Ink_Test_2;
 
-    public float _Width;
+/*    public float _Width;
     public float _NoiseScaleWidth;
-    public float _StrengthWidth;
+    public float _StrengthWidth;*/
 
-    public float _Height;
+/*    public float _Height;
     public float _NoiseScaleHeight;
-    public float _StrengthHeight;
-
+    public float _StrengthHeight;*/
+/*
     public float _MinHeightAndWidth = 0;
-    public float _MaxHeightAndWidth = 0.4f;
+    public float _MaxHeightAndWidth = 0.4f;*/
 
+    public float _Voronoi_Increase_Rate = 0.01f;
+    public float _Voronoi_Height = 0;
+    public float _Voronoi_Width = 0;
+    public float _Voronoi_Blotch = 0;
+    float _Voronoi_Increase;
     //public float _MinScaleHeightAndWidth = 30;
     //public float _MaxScaleHeightAndWidth = 80;
 
@@ -38,14 +43,14 @@ public class Ink_Test_3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        WidthRand();
-        HeightRand();
+/*        WidthRand();
+        HeightRand();*/
 
         //NoiseScaleWidthRand();
         //NoiseScaleHeightRand();
 
-        _WidthCount = (_MaxHeightAndWidth - _MinHeightAndWidth) * 0.3f;
-        _HeightCount = (_MaxHeightAndWidth - _MinHeightAndWidth) * 0.3f;
+        /*_WidthCount = (_MaxHeightAndWidth - _MinHeightAndWidth) * 0.3f;
+        _HeightCount = (_MaxHeightAndWidth - _MinHeightAndWidth) * 0.3f;*/
 
         //_NoiseScaleWidthCount = (_MaxScaleHeightAndWidth - _MinScaleHeightAndWidth) * 0.3f;
         //_NoiseScaleHeightCount = (_MaxScaleHeightAndWidth - _MinScaleHeightAndWidth) * 0.3f;
@@ -54,57 +59,63 @@ public class Ink_Test_3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Count Up/Down Start
+        _Voronoi_Increase = _Voronoi_Increase_Rate * Time.deltaTime;
+        _Voronoi_Width += _Voronoi_Increase;
+        _Voronoi_Height += _Voronoi_Increase;
+        _Voronoi_Blotch += _Voronoi_Increase;
+        Ink_Test_2.SetFloat("_Width_Voronoi_Offset", _Voronoi_Width);
+        Ink_Test_2.SetFloat("_Height_Voronoi_Offset", _Voronoi_Height);
+        Ink_Test_2.SetFloat("_Transparency_Voronoi_Offset", _Voronoi_Blotch);
+        /* //Count Up/Down Start
+         //Width Start
+         if (_WidthCountUp == true)
+         {
+             _WidthCount = _WidthCount + _HeightAndWidthIncrease;
+             Ink_Test_2.SetFloat("_Width", _WidthCount);
+         }
 
-        //Width Start
-        if (_WidthCountUp == true)
-        {
-            _WidthCount = _WidthCount + _HeightAndWidthIncrease;
-            Ink_Test_2.SetFloat("_Width", _WidthCount);
-        }
+         if (_WidthCountUp == false)
+         {
+             _WidthCount = _WidthCount - _HeightAndWidthIncrease;
+             Ink_Test_2.SetFloat("_Width", _WidthCount);
+         }
+         //Width End
+         //Height Start
+         if (_HeightCountUp == true)
+         {
+             _HeightCount = _HeightCount + _HeightAndWidthIncrease;
+             Ink_Test_2.SetFloat("_Height", _HeightCount);
+         }
 
-        if (_WidthCountUp == false)
-        {
-            _WidthCount = _WidthCount - _HeightAndWidthIncrease;
-            Ink_Test_2.SetFloat("_Width", _WidthCount);
-        }
-        //Width End
-        //Height Start
-        if (_HeightCountUp == true)
-        {
-            _HeightCount = _HeightCount + _HeightAndWidthIncrease;
-            Ink_Test_2.SetFloat("_Height", _HeightCount);
-        }
+         if (_HeightCountUp == false)
+         {
+             _HeightCount = _HeightCount - _HeightAndWidthIncrease;
+             Ink_Test_2.SetFloat("_Height", _HeightCount);
+         }
 
-        if (_HeightCountUp == false)
-        {
-            _HeightCount = _HeightCount - _HeightAndWidthIncrease;
-            Ink_Test_2.SetFloat("_Height", _HeightCount);
-        }
+         //Width Start
+         if(_WidthCountUp == true && _WidthCount > _Width)
+         {
+             WidthRand();
+         }
 
-        //Width Start
-        if(_WidthCountUp == true && _WidthCount > _Width)
-        {
-            WidthRand();
-        }
-
-        if (_WidthCountUp == false && _WidthCount < _Width)
-        {
-            WidthRand();
-        }
-        //Width End
-        //Height Start
-        if(_HeightCountUp == true && _HeightCount > _Height)
-        {
-            HeightRand();
-        }
-        if (_HeightCountUp == false && _HeightCount < _Height)
-        {
-            HeightRand();
-        }
+         if (_WidthCountUp == false && _WidthCount < _Width)
+         {
+             WidthRand();
+         }
+         //Width End
+         //Height Start
+         if(_HeightCountUp == true && _HeightCount > _Height)
+         {
+             HeightRand();
+         }
+         if (_HeightCountUp == false && _HeightCount < _Height)
+         {
+             HeightRand();
+         }*/
     }
 
-    void WidthRand()
+/*    void WidthRand()
     {
         _Width = Random.Range(_MinHeightAndWidth, _MaxHeightAndWidth);
 
@@ -130,31 +141,5 @@ public class Ink_Test_3 : MonoBehaviour
         {
             _HeightCountUp = true;
         }
-    }
-
-    //void NoiseScaleWidthRand()
-    //{
-    //    _NoiseScaleWidth = Random.Range(_MinScaleHeightAndWidth, _MaxScaleHeightAndWidth);
-    //    if (_Width < _WidthCount)
-    //    {
-    //        _NoiseScaleWidthCountUp = false;
-    //    }
-    //    else
-    //    {
-    //        _NoiseScaleWidthCountUp = true;
-    //    }
-    //}
-
-    //void NoiseScaleHeightRand()
-    //{
-    //    _NoiseScaleHeight = Random.Range(_MinScaleHeightAndWidth, _MaxScaleHeightAndWidth);
-    //    if (_Width < _WidthCount)
-    //    {
-    //        _NoiseScaleHeightCountUp = false;
-    //    }
-    //    else
-    //   {
-    //       _NoiseScaleHeightCountUp = true;
-    //    }
-    //}
+    }*/
 }
